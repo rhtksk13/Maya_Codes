aad5af7666986c8593d45f6abc140549b60f5c5c
rhtksk13 <43501436+rhtksk13@users.noreply.github.com>
2026-04-18 20:59:34 +0530

    Update PoleVectorPositionFinder.py
    Added Comments

--- PoleVectorPositionFinder.py
+++ PoleVectorPositionFinder.py
@@ -4,39 +4,48 @@ import maya.cmds as cmds
 import sys
 
 """
+Created by - Rohit Kumar Singh Kushwaha
+Email - rhtksk13@gmail.com
+
+Example:
+cmds.findPoleVector(sj = "start_Jnt", mj = "mid_Jnt", ej = "end_Jnt")
+
+This custom command finds the position for pole vector to be Placed in an ik chain.
+
 - Requirements:
 
-    1. Start Joint
-    2. Mid Joint
-    3. End Joint
+    1. Start Joint Flag
+    2. Mid Joint Flag
+    3. End Joint Flag
     4. Calculation
     5. A locator created at the position for poleVector
 """
-cmdName = "findPoleVector"
-kStartFlag = "-sj"
-kStartLongFlag = "-startJoint"
+cmdName = "findPoleVector" # command Name - cmds.findPoleVector()
+kStartFlag = "-sj" # short name for flag
+kStartLongFlag = "-startJoint" # Long name for flag
 
-kMidFlag = "-mj"
-kMidLongFlag = "-midJoint"
+kMidFlag = "-mj" # short name for flag
+kMidLongFlag = "-midJoint" # Long name for flag
 
-kEndFlag = "-ej"
-kEndLongFlag = "-endJoint"
+kEndFlag = "-ej" # short name for flag
+kEndLongFlag = "-endJoint" # Long name for flag
 
-kHelpFlag = "-h"
-kHelpLongFlag = "-help"
+kHelpFlag = "-h" # short name for help flag
+kHelpLongFlag = "-help" # Long name for help flag
 
-message = "This command helps in finding the position for Pole Vector to be placed provided three joints of a 3 chain setup."
+message = "This command helps in finding the position for Pole Vector to be placed provided three joints of a 3 chain setup." # message shown by help flag
 kSuccess = 1
 kFail = 0
-
+# Class creation for command
 class poleVector(ompx.MPxCommand):
+    # setting up variables for joints as values
     startSparse = None
     midSparse = None
     endSparse = None
-
+    # constructor
     def __init__(self):
         ompx.MPxCommand.__init__(self)
-
+    # parsing the arguments
     def argumentParse(self, argList):
         syntax = self.syntax()
         mArgDataBase = om.MArgDatabase(syntax, argList)
@@ -59,28 +68,28 @@ class poleVector(ompx.MPxCommand):
             self.setResult(message)
 
     def redoIt(self):
-        startpos = cmds.xform(self.startSparse, q = True, rp = True, ws = True)
+        startpos = cmds.xform(self.startSparse, q = True, rp = True, ws = True) # Getting the world position of the start joint
         startVector = om.MVector(startpos[0], startpos[1], startpos[2])
-        #startVector = cmds.xform(self.startSparse, q = True, t = True, ws = True)
-        midPos = cmds.xform(self.midSparse, q = True, rp = True, ws = True)
+        
+        midPos = cmds.xform(self.midSparse, q = True, rp = True, ws = True) # Getting the world position of the mid joint
         midVector = om.MVector(midPos[0], midPos[1], midPos[2])
-        #startVector = cmds.xform(self.midSparse, q = True, t = True, ws = True)
-        endPos = cmds.xform(self.endSparse, q = True, rp = True, ws = True)
+        
+        endPos = cmds.xform(self.endSparse, q = True, rp = True, ws = True) # Getting the world position of the end joint
         endVector = om.MVector(endPos[0], endPos[1], endPos[2])
-        #startVector = cmds.xform(self.endSparse, q = True, t = True, ws = True)
-
+        
+        # Finding the mid Point
         midPointVector = om.MVector()
         midPointVector = (startVector + endVector) / 2
-
+        # Finding the direction
         directionVector = om.MVector()
         directionVector = midVector - midPointVector
-
+        # Finding the destination for pole vector
         destVector = om.MVector()
         destVector = midPointVector + (directionVector * 4)
-
+        # Creating the loacator at the destination
         polelocator = cmds.spaceLocator(n = "Pole_Vector_position_Loc")
         cmds.xform(polelocator, t = (destVector.x, destVector.y, destVector.z), ws = True)
-        #cmds.xform(polelocator, t = destVector, ws = True)
+        
         return kSuccess
 
     def doIt(self, argList):
@@ -101,14 +110,14 @@ def syntaxCreator():
 
 def cmdCreator():
     return ompx.asMPxPtr(poleVector())
-
+# Initializing the Plugin
 def initializePlugin(MObject):
     mPlugin = ompx.MFnPlugin(MObject)
     try:
         mPlugin.registerCommand(cmdName, cmdCreator, syntaxCreator)
     except:
         sys.stderr.write("Failed to Register Command: "+cmdName)
-
+# UnInitializing the Plugin
 def uninitializePlugin(MObject):
     mPlugin = ompx.MFnPlugin(MObject)
     try:
